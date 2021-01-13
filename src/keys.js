a2d13f0fb5a5f0438f6e6dc5aa2ed45fdacd1a14
emunera <edisonupb2013@gmail.com>
2021-01-12 19:54:25 -0500

    solucion db

--- src/keys.js
+++ src/keys.js
@@ -7,10 +7,10 @@ module.exports = {
     // }
 
     database: {
-      host: 'ec2-52-204-113-104.compute-1.amazonaws.com',
-          user: 'nfuvpcisleiirt',
-          password: '8dea24ddfaa8e8ed13004381ace4af09011694341b003f0abeb0ec599dc7546c',
-          database: 'd40flvhb5ru8e7'
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'be7135c7eb02c0',
+        password: '57475ea8',
+        database: 'heroku_cea3c0fd4f250a2'
     }
 };
 
